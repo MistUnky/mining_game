8e1c0166ce8d580c42c7b2641bfcc12a111f846e
MistUnky <misterunknown128@gmail.com>
2021-04-09 22:20:59 -0400

    Replace normal mapgen with random walk mapgen.

--- mods/base/init.lua
+++ mods/base/init.lua
@@ -93,7 +93,7 @@ minetest.register_node("base:mud", {
 		}
 	},
 })
-
+--[[
 minetest.register_alias("mapgen_stone", "base:mud")
 minetest.register_alias("mapgen_water_source", "air")
 minetest.register_alias("mapgen_river_water_source", "air")
@@ -120,3 +120,68 @@ minetest.register_alias("mapgen_cobble", "air")
 minetest.register_alias("mapgen_stair_cobble", "air")
 minetest.register_alias("mapgen_mossycobble", "air")
 minetest.register_alias("mapgen_stair_desert_stone", "air")
+--]]
+
+aleph = {}
+beth = {}
+gamma = {}
+delta = {}
+rwalkmove = function(height)
+	if math.random(0,1) == 1 then
+		height = height + 1
+	else
+		height = height - 1
+	end
+	return height
+end
+minetest.register_on_mapgen_init(function(mapgen_params)
+	local h = 0
+	math.randomseed(mapgen_params.seed)
+	for i = 0,31000 do
+		h = rwalkmove(h)
+		aleph[i] = h
+	end
+	h = 0
+	for i = 0,-31000,-1 do
+		h = rwalkmove(h)
+		aleph[i] = h
+	end
+	h = 0
+	for i = 0,31000 do
+		h = rwalkmove(h)
+		beth[i] = h
+	end
+	h = 0
+	for i = 0,-31000,-1 do
+		h = rwalkmove(h)
+		beth[i] = h
+	end
+end)
+minetest.set_mapgen_params({mgname="singlenode"})
+minetest.register_on_generated(function(minp, maxp, seed)
+	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+	local data = vm:get_data()
+	local a = VoxelArea:new({MinEdge = emin, MaxEdge = emax})
+	local csize = vector.add(vector.subtract(maxp, minp), 1)
+	local write = false
+	for z = minp.z, maxp.z do
+	for y = minp.y, maxp.y do
+	for x = minp.x, maxp.x do
+		local ivm = a:index(x, y, z)
+		if y < aleph[x] + beth[z] then --ore gen -- gravel sand clay
+			data[ivm] = minetest.get_content_id("base:mud")
+			write = true
+		end
+	end
+	end
+	end
+	
+	if write then
+		vm:set_data(data)
+		vm:set_lighting({day = 0, night = 0})
+		vm:calc_lighting()
+		vm:update_liquids()
+		vm:write_to_map()
+	end
+
+end)
